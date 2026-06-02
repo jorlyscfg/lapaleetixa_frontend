e41ac41e9b00d28968e08b11bf9e07d9c24ba12b
Jegdev <admin@lapaletixa.com>
2026-06-02 20:26:16 +0000

    chore: fallback to BACKEND_URL in config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://lapaletixa.local:8080";
+const BACKEND_URL = process.env.BACKEND_URL || process.env.NEXT_PUBLIC_BACKEND_URL || "http://lapaletixa.local:8080";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.87", "lapaletixa.local", "lapaletixa.localhost", "lapaletixa.jegdev.com"],
